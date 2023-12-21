4ab4041f0a3752129cc098ec616679b34fcf3e56
Lycons-rage <chinmayatewari.20@gmail.com>
2023-12-21 20:54:10 +0530

    Day 2 of scrapper prototype. 20:54 21-12-2023

--- Scrapper/main.py
+++ Scrapper/main.py
@@ -9,6 +9,18 @@ from urllib.request import urlopen as uo
 app = Flask(__name__)
 
 
+# landing page
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+# result page
+@app.route("/reviews", methods=["GET","POST"])
+def reviews():
+    if request.method == "POST":
+        product_name = request.form["product_name"]
+        return product_name
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=7000)
